b228ea11e6ae6297175a6da3394a79638a3d396f
Andrew Jones <ajon542@gmail.com>
2015-06-13 14:24:33 -0700

    Added basic n^3 matrix multiply

--- MatrixMultiplication/app.js
+++ MatrixMultiplication/app.js
@@ -1,1 +1,32 @@
-﻿console.log('Hello world');
+﻿
+function CreateMatrix(rows) {
+    var arr = [];
+    
+    for (var i = 0; i < rows; i++) {
+        arr[i] = [];
+    }
+    
+    return arr;
+}
+
+function SquareMatrixMultiply(a, b) {
+    
+    var matrixSize = a[0].length;
+    //var c = CreateMatrix(matrixSize);
+    var c = [[0, 0], [0, 0]];
+    for (var i = 0; i < matrixSize; ++i) {
+        for (var j = 0; j < matrixSize; ++j) {
+            c[i][j] = 0;
+            for (var k = 0; k < matrixSize; ++k) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    return c;
+}
+
+var a = [[1, 2], [3, 4]];
+var b = [[1, 2], [3, 4]];
+var c = SquareMatrixMultiply(a, b);
+console.log(c);
