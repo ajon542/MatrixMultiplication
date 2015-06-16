0c1a01f6401d1cdcb927ff8b5a007eb84ff4d1bb
Andrew Jones <ajon542@gmail.com>
2015-06-16 08:13:53 -0700

    Implement Strassen's matrix multiplication

--- MatrixMultiplication/app.js
+++ MatrixMultiplication/app.js
@@ -48,6 +48,20 @@ function MatrixAddition(a, b) {
     return c;
 }
 
+function MatrixSubtraction(a, b) {
+    
+    var n = a[0].length;
+    var c = CreateMatrix(n);
+    
+    for (var i = 0; i < n; ++i) {
+        for (var j = 0; j < n; ++j) {
+            c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    
+    return c;
+}
+
 function SquareMatrixMultiply(a, b) {
     
     var n = a[0].length;
@@ -98,10 +112,65 @@ function SquareMatrixMultiply_Recursive(a, b) {
     return c;
 }
 
+function SquareMatrixMultiply_Strassen(a, b) {
+
+    var n = a[0].length;
+    var c = CreateMatrix(n);
+    
+    if (n == 1) {
+        c[0][0] = a[0][0] * b[0][0];
+    } else {
+        
+        var a11 = GetMatrixData(a, n / 2, 0, 0);
+        var a12 = GetMatrixData(a, n / 2, n / 2, 0);
+        var a21 = GetMatrixData(a, n / 2, 0, n / 2);
+        var a22 = GetMatrixData(a, n / 2, n / 2, n / 2);
+        
+        var b11 = GetMatrixData(b, n / 2, 0, 0);
+        var b12 = GetMatrixData(b, n / 2, n / 2, 0);
+        var b21 = GetMatrixData(b, n / 2, 0, n / 2);
+        var b22 = GetMatrixData(b, n / 2, n / 2, n / 2);
+
+        var s1 = MatrixSubtraction(b12, b22);
+        var s2 = MatrixAddition(a11, a12);
+        var s3 = MatrixAddition(a21, a22);
+        var s4 = MatrixSubtraction(b21, b11);
+        var s5 = MatrixAddition(a11, a22);
+        var s6 = MatrixAddition(b11, b22);
+        var s7 = MatrixSubtraction(a12, a22);
+        var s8 = MatrixAddition(b21, b22);
+        var s9 = MatrixSubtraction(a11, a21);
+        var s10 = MatrixAddition(b11, b12);
+
+        var p1 = SquareMatrixMultiply_Strassen(a11, s1);
+        var p2 = SquareMatrixMultiply_Strassen(s2, b22);
+        var p3 = SquareMatrixMultiply_Strassen(s3, b11);
+        var p4 = SquareMatrixMultiply_Strassen(a22, s4);
+        var p5 = SquareMatrixMultiply_Strassen(s5, s6);
+        var p6 = SquareMatrixMultiply_Strassen(s7, s8);
+        var p7 = SquareMatrixMultiply_Strassen(s9, s10);
+
+        var c11 = MatrixAddition(MatrixSubtraction(MatrixAddition(p5, p4), p2), p6);
+        var c12 = MatrixAddition(p1, p2);
+        var c21 = MatrixAddition(p3, p4);
+        var c22 = MatrixSubtraction(MatrixSubtraction(MatrixAddition(p5, p1), p3), p7);
+        
+        SetMatrixData(c11, c, c11[0].length, 0, 0);
+        SetMatrixData(c12, c, c11[0].length, n / 2, 0);
+        SetMatrixData(c21, c, c11[0].length, 0, n / 2);
+        SetMatrixData(c22, c, c11[0].length, n / 2, n / 2);
+    }
+    
+    return c;
+}
+
 var a = [[1, 2, 3, 4], [5, 6, 7, 8], [-10, -2, -4, -60], [-1, -2, -3, -4]];
 var b = [[1, 2, 3, 4], [5, 6, 7, 8], [-10, -2, -4, -60], [-1, -2, -3, -4]];
 var c = SquareMatrixMultiply(a, b);
 var d = SquareMatrixMultiply_Recursive(a, b);
+var e = SquareMatrixMultiply_Strassen(a, b);
 
 console.log(c);
 console.log(d);
+console.log(e);
+
